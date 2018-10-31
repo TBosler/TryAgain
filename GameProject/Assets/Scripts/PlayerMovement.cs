7e33a6aa0d33472f201fc859039e63b1038cfdf2
TBosler <tbosler95@gmail.com>
2018-10-30 21:58:00 -0400

    Moving Character

--- GameProject/Assets/Scripts/PlayerMovement.cs
+++ GameProject/Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,26 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public float runspeed = 40f;
+    public float screenTop = 0f;
+    public float screenBot = 0f;
     private Vector3 velocity = Vector3.zero;
+    float horizontalMove = 0f;
+    float verticalMove = 0f;
     private bool m_FacingRight = true;  // For determining which way the player is currently facing.
     [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// How much to smooth out the movement
 
     private Rigidbody2D m_Rigidbody2D;
+
+    private void Update()
+    {
+       horizontalMove   = Input.GetAxisRaw("Horizontal") * runspeed;
+       verticalMove     = Input.GetAxisRaw("Vertical") * runspeed;
+    }
+
+    private void FixedUpdate()
+    {
+        Move(horizontalMove * Time.fixedDeltaTime, verticalMove * Time.fixedDeltaTime);
+    }
 
     private void Awake()
     {
@@ -17,22 +32,34 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     // Update is called once per frame
-    public void Move(float move)
+    public void Move(float horz, float vert)
     {
-        // Move the character by finding the target velocity
-        Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
 
-        // And then smoothing it out and applying it to the character
-        m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref velocity, m_MovementSmoothing);
+        // Move the character by finding the target velocity
+        /*Vector3 targetVelocity = new Vector2(horz * 10f, vert * 10f);//m_Rigidbody2D.velocity.y);
+        Debug.Log(targetVelocity.y);
+        Debug.Log(transform.position.y);
+        if (transform.position.y + targetVelocity.y > screenTop)
+            targetVelocity.y = 0;
+            
+         // And then smoothing it out and applying it to the character
+        m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref velocity, m_MovementSmoothing);*/
+
+        float newX = transform.position.x + horz;
+        float newY = transform.position.y + vert;
+
+        newY = Mathf.Clamp(newY, screenBot, screenTop);
+
+        transform.position = new Vector3(newX, newY, 0);
 
         // If the input is moving the player right and the player is facing left...
-        if (move > 0 && !m_FacingRight)
+        if (horz > 0 && !m_FacingRight)
         {
             // ... flip the player.
             Flip();
         }
         // Otherwise if the input is moving the player left and the player is facing right...
-        else if (move < 0 && m_FacingRight)
+        else if (horz < 0 && m_FacingRight)
         {
             // ... flip the player.
             Flip();
