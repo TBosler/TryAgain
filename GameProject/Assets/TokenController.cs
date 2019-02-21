a82686e5194d7893f1e59c937f46e68d90e13780
TBosler <tbosler95@gmail.com>
2019-02-20 21:23:47 -0500

    Update TokenController.cs

--- GameProject/Assets/TokenController.cs
+++ GameProject/Assets/TokenController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class TokenController : MonoBehaviour
 {
     public GameObject playerCharacter;
     // Start is called before the first frame update
